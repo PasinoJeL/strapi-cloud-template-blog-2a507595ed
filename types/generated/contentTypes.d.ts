2363809d6333c2d4a14565958670b8e24c43dd1d
JSchoemacker <jschoemacker@pasino.ch>
2024-04-18 09:28:51 +0200

    more_button field added on casinoGames CT

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1049,6 +1049,12 @@ export interface ApiCasinoGameCasinoGame extends Schema.CollectionType {
           localized: true;
         };
       }>;
+    more_button: Attribute.String &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
